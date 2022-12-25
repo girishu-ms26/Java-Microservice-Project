f69afbcf2565fd285693d5525c458585453231c5
Girishu M S <girishums1999@gmail.com>
2022-12-25 18:09:14 +0530

    Initial Commit

--- at0m-cloud-gateway/src/main/java/com/at0m/productmanagement/at0mcloudgateway/At0mCloudGatewayApplication.java
+++ at0m-cloud-gateway/src/main/java/com/at0m/productmanagement/at0mcloudgateway/At0mCloudGatewayApplication.java
@@ -2,8 +2,10 @@ package com.at0m.productmanagement.at0mcloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class At0mCloudGatewayApplication {
 
     public static void main(String[] args) {
