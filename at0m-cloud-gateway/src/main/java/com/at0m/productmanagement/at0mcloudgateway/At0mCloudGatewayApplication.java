d28fb1fb39f94e7d4c3642e74156f8c115a6c1dc
Girishu M S <girishums1999@gmail.com>
2022-12-25 17:24:30 +0530

    Initial Commit

---
+++ at0m-cloud-gateway/src/main/java/com/at0m/productmanagement/at0mcloudgateway/At0mCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.at0m.productmanagement.at0mcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class At0mCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(At0mCloudGatewayApplication.class, args);
+    }
+
+}
---
+++ at0m-cloud-gateway/src/test/java/com/at0m/productmanagement/at0mcloudgateway/At0mCloudGatewayApplicationTests.java
@@ -0,0 +1,8 @@
+package com.at0m.productmanagement.at0mcloudgateway;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class At0mCloudGatewayApplicationTests {
+
+}
